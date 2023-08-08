df8d27f65f7c06d2580524fead60d72cca3f25db
asviswesh <130618325+asviswesh@users.noreply.github.com>
2023-08-08 16:06:43 -0700

    Running MLDE on one-hot encodings of sequences in Dallago et. al

---
+++ mlde_test.py
@@ -0,0 +1,9 @@
+from train_eval import *
+
+save_path_base = '/home/annika/mlde/gb1_results/'
+
+
+mlde_sim = MLDESim(save_path=save_path_base, encoding='one-hot', model_class='cnn', n_samples=149360, train_name='gb1_train.csv',
+                   test_name='gb1_test.csv', validation_name='k-fold', first_append=True, feat_to_predict='Fitness', neural_network=True)
+# mlde_sim.train_all()
+mlde_sim.run_neural_network(learning_rate=0.001, num_epochs=20)
