70b58bec92113d091f0868b75171e09086431696
asviswesh <130618325+asviswesh@users.noreply.github.com>
2023-08-26 21:51:44 -0700

    Added parser for base path for test/train csv creation.

--- create_train_test_csv.py
+++ create_train_test_csv.py
@@ -1,13 +1,12 @@
+import argparse
 import csv
 
-def create_train_dataset(sheet_name):
-    base_path = '/home/annika/mlde/'
+def create_train_dataset(base_path, sheet_name):
     csv_path = base_path + sheet_name
     if '.csv' not in sheet_name:
         raise ValueError("Data is not in the .csv file.")
     with open(csv_path, 'w') as train_csv:
         col_names = ['Variants', 'Fitness', 'sequence']
-        print(col_names)
         writer = csv.DictWriter(train_csv, fieldnames=col_names)
         writer.writeheader()
         with open('/home/annika/mlde/four_mutations_full_data.csv', 'r') as full_data:
@@ -31,7 +30,7 @@ def create_train_dataset(sheet_name):
                     writer.writerow(line)
 
 
-def create_test_dataset(sheet_name, need_fitness):
+def create_test_dataset(base_path, sheet_name, need_fitness):
     base_path = '/home/annika/mlde/'
     csv_path = base_path + sheet_name
     if '.csv' not in sheet_name:
@@ -43,7 +42,7 @@ def create_test_dataset(sheet_name, need_fitness):
         with open('/home/annika/mlde/four_mutations_full_data.csv', 'r') as full_data:
             reader_obj = csv.DictReader(full_data)
             for line in reader_obj:
-                if line.get('HD') == '2':
+                if line.get('HD') != '0' or line.get('HD') == 1:
                     if need_fitness:
                         line.pop("Fitness")
                     line.pop('HD')
@@ -62,6 +61,10 @@ def create_test_dataset(sheet_name, need_fitness):
                     line.pop('low_vs_high_validation')
                     writer.writerow(line)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("base_path")
+args = parser.parse_args()
+base_path = args.base_path
 
 create_train_dataset('gb1_train.csv')
 create_test_dataset('gb1_test.csv', True)
